5a0fa9f8e76d69a935d398d43d50a78a14d4af5c
Yashin Soobramoney <ysoobramoney@gmail.com>
2019-08-15 23:34:58 +0200

    ASP Login
    Roles and Visibility added

--- EmployeePages/EmployeeAccount.aspx.cs
+++ EmployeePages/EmployeeAccount.aspx.cs
@@ -9,6 +9,6 @@ public partial class EmployeePages_EmployeeAccount : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        PageMenu.Initialise(Page); 
     }
 }
