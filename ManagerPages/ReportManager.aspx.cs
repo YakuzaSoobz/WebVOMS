9c145193c4b972ebdfba5ee58e35d2bc9a5f9cb7
Sharad Singh <sheru2012@gmail.com>
2019-10-12 23:26:56 +0200

    Web Reports
    Are Klaar

--- ManagerPages/ReportManager.aspx.cs
+++ ManagerPages/ReportManager.aspx.cs
@@ -27,9 +27,110 @@ public partial class ManagerPages_ReportManager : System.Web.UI.Page
         {
              if (DropDownListReportSelect.SelectedIndex == 0)
             {
+                
                 ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
             }
             else if (DropDownListReportSelect.SelectedIndex == 0)
+            {
+                ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
+            }
+        }
+        catch (Exception exception)
+        {
+            LabelError.Visible = true;
+            LabelError.Text = "Oops, an error was encountered while loading this page. Our team is looking at the issue! Please retry later!";
+        }
+    }
+    protected void ViewReportButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (DropDownListReportSelect.SelectedIndex == 0)
+            {
+
+                Response.Redirect("~/TotalProductSalesForm.aspx");
+            }
+            else if (DropDownListReportSelect.SelectedIndex == 0)
+            {
+                ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
+            }
+
+            if (DropDownListReportSelect.SelectedIndex == 1)
+            {
+
+                Response.Redirect("~/TotalValueOfSalesForSelectMonth.aspx");
+            }
+            else if (DropDownListReportSelect.SelectedIndex == 1)
+            {
+                ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
+            }
+
+            if (DropDownListReportSelect.SelectedIndex == 2)
+            {
+
+                Response.Redirect("~/TotalSupplierCostPerSupplierForm.aspx");
+            }
+            else if (DropDownListReportSelect.SelectedIndex == 2)
+            {
+                ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
+            }
+
+            if (DropDownListReportSelect.SelectedIndex == 3)
+            {
+
+                Response.Redirect("~/EmployeeSalesPerProduct.aspx");
+            }
+            else if (DropDownListReportSelect.SelectedIndex == 3)
+            {
+                ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
+            }
+
+            if (DropDownListReportSelect.SelectedIndex == 4)
+            {
+
+                Response.Redirect("~/TotalSalesPerSuburbForm.aspx");
+            }
+            else if (DropDownListReportSelect.SelectedIndex == 4)
+            {
+                ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
+            }
+
+            if (DropDownListReportSelect.SelectedIndex == 5)
+            {
+
+                Response.Redirect("~/AmountOfProductsSoldPerEmployeeForLastYearForm.aspx");
+            }
+            else if (DropDownListReportSelect.SelectedIndex == 5)
+            {
+                ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
+            }
+
+            if (DropDownListReportSelect.SelectedIndex == 6)
+            {
+
+                Response.Redirect("~/ProductsSoldPerMonthForLastYearForm.aspx");
+            }
+            else if (DropDownListReportSelect.SelectedIndex == 6)
+            {
+                ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
+            }
+
+            if (DropDownListReportSelect.SelectedIndex == 7)
+            {
+
+                Response.Redirect("~/QuotationsPerMonth.aspx");
+            }
+            else if (DropDownListReportSelect.SelectedIndex == 7)
+            {
+                ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
+            }
+
+            if (DropDownListReportSelect.SelectedIndex == 8)
+            {
+
+                Response.Redirect("~/CustomerIncome.aspx");
+            }
+            else if (DropDownListReportSelect.SelectedIndex == 8)
             {
                 ReportDescription.InnerHtml = "Report Ojndadfsssssssssssssssssssssssss ssssssssssssssss  sdddddddddddddddddddddddddd  ddddddddddd";
             }
---
+++ TotalProductSalesForm.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class TotalProductSalesForm : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TestDataSetShankar DS = new TestDataSetShankar();
+        TestDataSetShankarTableAdapters.CustomerTableAdapter CustTA = new TestDataSetShankarTableAdapters.CustomerTableAdapter();
+        TestDataSetShankarTableAdapters.ProductTableAdapter ProdTA = new TestDataSetShankarTableAdapters.ProductTableAdapter();
+        TestDataSetShankarTableAdapters.Customer_QuoteTableAdapter CusQTA = new TestDataSetShankarTableAdapters.Customer_QuoteTableAdapter();
+        TestDataSetShankarTableAdapters.Customer_Quote_Line_ItemTableAdapter CustQlTA = new TestDataSetShankarTableAdapters.Customer_Quote_Line_ItemTableAdapter();
+
+
+        CustQlTA.Fill(DS.Customer_Quote_Line_Item);
+        CusQTA.Fill(DS.Customer_Quote);
+        ProdTA.Fill(DS.Product);
+
+        CrystalReportSource1.ReportDocument.SetDataSource(DS);
+    }
+    protected void CrystalReportViewer1_Init(object sender, EventArgs e)
+    {
+
+    }
+}
